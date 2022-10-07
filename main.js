578c1c37a7e57e601a0fb4811474b45d1712c6ad
fannyUnab <fanny.casadiego2@o365.unab.edu.co>
2022-10-06 22:53:57 -0500

    creado en clase

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+function funcionColor(newColor){
+    let elemento=document.getElementById('titulonosotros');
+    elemento.style.color=newColor;
+}
+
+
+function funcionprod(){
+    document.getElementById('product').innerHTML="nuevo texto";
+}
+
+function otrafuncion(){
+    var x=document.getElementById('uno').innerHTML;
+    document.getElementById('dos').innerHTML=x;
+
+}
